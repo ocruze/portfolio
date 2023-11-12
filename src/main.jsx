579a4e44657ecb92a38163d9a8192ad61f2a1bc8
Orka Arnest Cruze <o.cruze@live.com>
2023-11-12 16:50:05 +0100

    build: upgrade to react 18

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import "./assets/styles/index.scss";
 import "bootstrap";
+import "./assets/styles/index.scss";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./i18n";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
